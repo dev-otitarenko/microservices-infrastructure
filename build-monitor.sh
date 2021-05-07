de6d2645a52ae47e8d62550607e665cf60ae24be
oleksii titarenko <atitarenko.eng@gmail.com>
2021-05-07 19:10:12 +0200

    project: minor changes

--- build-monitor.sh
+++ build-monitor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose-monitor.yml -p ms-practice stop
-docker-compose -f docker-compose-monitor.yml -p ms-practice rm
-docker-compose -f docker-compose-monitor.yml -p ms-practice up -d --build
+docker-compose -f ./docker-compose-monitor.yml -p ms-practice stop
+docker-compose -f ./docker-compose-monitor.yml -p ms-practice rm
+docker-compose -f ./docker-compose-monitor.yml -p ms-practice up -d --build
