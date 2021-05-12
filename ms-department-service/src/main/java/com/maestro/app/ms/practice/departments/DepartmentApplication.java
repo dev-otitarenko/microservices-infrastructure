f5b50b684c76979a78c9499e652d1220636c2823
oleksii titarenko <atitarenko.eng@gmail.com>
2021-05-12 14:42:59 +0200

    department-service: 1. removed openfeign 2, removed resilience4j 3. removed DepartmentDetailsController.java

--- ms-department-service/src/main/java/com/maestro/app/ms/practice/departments/DepartmentApplication.java
+++ ms-department-service/src/main/java/com/maestro/app/ms/practice/departments/DepartmentApplication.java
@@ -2,10 +2,8 @@ package com.maestro.app.ms.practice.departments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class DepartmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentApplication.class, args);
--- ms-department-service/src/main/java/com/maestro/app/ms/practice/departments/controllers/Resilience4jConstants.java
+++
@@ -1,9 +0,0 @@
-package com.maestro.app.ms.practice.departments.controllers;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class Resilience4jConstants {
-    static final String PERSON_CLIENT = "personClient";
-}
