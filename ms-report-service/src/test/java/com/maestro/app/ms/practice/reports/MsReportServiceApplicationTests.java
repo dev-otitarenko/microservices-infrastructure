684bd3c8d08b589160def79097903dee4116360e
oleksii titarenko <atitarenko.eng@gmail.com>
2021-05-12 11:37:10 +0200

    report-service: added support mvnw

---
+++ ms-report-service/src/test/java/com/maestro/app/ms/practice/reports/MsReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maestro.app.ms.practice.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
