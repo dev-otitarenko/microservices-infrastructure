42c7330af72e9de2aa54d80fffd71d8f28e321ab
dev-otitarenko <76594207+dev-otitarenko@users.noreply.github.com>
2021-05-16 18:38:50 +0200

    Develop (#14)
    * department-service: moved to another package
    
    * employee-service: moved to another package
    
    * report-service: moved to another package
    
    * report-service: moved to the internal project ./microservices-chapter-1
    
    * report-service: moved to the internal project ./microservices-chapter-1
    
    * small changes in pom.xml

---
+++ microservices-practice-1/ms-department-service/src/main/java/com/maestro/app/practice/ch1/ms/departments/controllers/DepartmentController.java
@@ -0,0 +1,54 @@
+package com.maestro.app.practice.ch1.ms.departments.controllers;
+
+import com.maestro.app.practice.ch1.ms.departments.entities.Department;
+import com.maestro.app.practice.ch1.ms.departments.services.DepartmentService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+public class DepartmentController {
+    private final DepartmentService deptService;
+
+    @GetMapping("/dept")
+    public List<Department> getList() {
+        log.info("Get the list of departments.");
+        return deptService.getList();
+    }
+
+    @GetMapping("/dept/{id}")
+    public Department get(@PathVariable Long id) {
+        log.info("Get the department record \"{}\".", id);
+        return deptService.get(id);
+    }
+
+    @DeleteMapping("/dept/{id}")
+    @Transactional
+    public void delete(@PathVariable Long id) {
+        log.info("Delete the specific department \"{}\".", id);
+        Department dept = deptService.get(id);
+        if (dept != null)
+            deptService.delete(dept);
+    }
+
+    @PutMapping("/dept")
+    @Transactional
+    public void create(@RequestBody @Valid Department dept) {
+        log.info("Create the department.");
+        deptService.save(dept);
+    }
+
+    @PostMapping("/dept/{id}")
+    @Transactional
+    public void update(@PathVariable Long id, @RequestBody @Valid Department dept) {
+        log.info("Update the specific department \"{}\".", id);
+        dept.setId(id);
+        deptService.save(dept);
+    }
+}
--- ms-department-service/src/main/java/com/maestro/app/ms/practice/departments/repositories/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maestro.app.ms.practice.departments.repositories;
-
-import com.maestro.app.ms.practice.departments.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
